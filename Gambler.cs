77a05930e7a19adb0e3535f789734d8ec3092c38
Shashikumar <shashirgowdas67@gmail.com>
2022-03-11 09:42:44 +0530

    Added:Player will win or loose

---
+++ Gambler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamblingSimulations
+{
+    internal class Gambler
+    {
+        public void winLoss()
+        {
+            Random random = new Random();
+            int value = random.Next(2);
+            if (value == 1)
+            {
+                Console.WriteLine("Gambler own");
+            }
+            else
+                Console.WriteLine("Gambler Loss");
+        }
+
+    }
+}
+    
+
--- Program.cs
+++ Program.cs
@@ -4,16 +4,14 @@ namespace GamblingSimulations
 {
     class Program
     {
-        double betPrice, stack;
-        int numberOfPlay;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to gambing simulation");
             int stakeHold = 100;
             const int bet = 1;
-            Console.ReadLine();
 
-         
+            Gambler Gambler = new Gambler();
+            Gambler.winLoss();
         }
     }
 
